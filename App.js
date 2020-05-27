e292f7f2e69df1dd205c653e185e20b70683d08f
Liz Kaufman <46270629+lizkaufman@users.noreply.github.com>
2020-05-27 15:36:09 +0100

    touchables

--- App.js
+++ App.js
@@ -1,17 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  TouchableHighlight,
+  View,
+  Image,
+  SafeAreaView,
+} from 'react-native';
 
 //view = div (for grouping or laying out children)
 //no html elements at all; need to build in react native elements
 //cross-platform: when compiled, React Native compiles these components into their native components for iOS/Android
 
-//essential text component props:
+//Essential text component props:
 // - numberOfLines = max # of lines to display (excess gets truncated w/ ...)
 // - onPress = event listener; function for what happens when the element is touched
 
+//Local images: require function w/ path as source, ex: source={require('./assets/icon.png')}
+//Network images: source w/ object w/ uri, ex: source={{uri: 'https://picsum.photos/200/300'}}
+
+//Touchable components have onPress and onLongPress events
+
+//Three cross-platform touchable components:
+// - TouchableWithoutFeedback (no feedback when touched)
+// - TouchableOpacity (component goes see-thru when touched)
+// - TouchableHighlight (component goes dark when touched)
+
 export default function App() {
   function handlePress() {
-    console.log('text pressed');
+    console.log('you pressed it');
+  }
+
+  function handleLongPress() {
+    console.log('you pressed it for a long time');
   }
 
   return (
@@ -20,7 +43,16 @@ export default function App() {
       <Text numberOfLines={1} onPress={handlePress}>
         Hello React Native!
       </Text>
-      <Text>From Liz x</Text>
+      {/*Image component doesn't have onPress! Need to wrap w/ separate touchable component depending on feedback we want to give to users. */}
+      <TouchableHighlight onPress={handlePress} onLongPress={handleLongPress}>
+        <Image
+          source={{
+            uri: 'https://picsum.photos/200/300',
+            width: 300,
+            height: 200, //for network images, have to specify dimensions before it'll display
+          }}
+        />
+      </TouchableHighlight>
     </SafeAreaView>
   );
 }
@@ -28,7 +60,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1, //view is flexible - can grow both horiz and vert to fill free space (1 is taking whole screen)
-    backgroundColor: 'dodgerblue',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
