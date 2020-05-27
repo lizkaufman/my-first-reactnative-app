d0527a80224fca937d9c2b247cf6f59dd8f3ab92
Liz Kaufman <46270629+lizkaufman@users.noreply.github.com>
2020-05-27 14:53:28 +0100

    expo initialized w/ blank template

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+//view is like div
+//when compiled, React Native compiles these components into their native components for iOS/Android
+
 export default function App() {
+  let x = 1;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello React Native!</Text>
+      <Text>From Liz x</Text>
     </View>
   );
 }
