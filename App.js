3184c64837f3f49f86f015ace7103719652634d7
Liz Kaufman <46270629+lizkaufman@users.noreply.github.com>
2020-05-28 14:50:16 +0100

    buttons and alerts

--- App.js
+++ App.js
@@ -8,6 +8,8 @@ import {
   View,
   Image,
   SafeAreaView,
+  Button,
+  Alert,
 } from 'react-native';
 
 //view = div (for grouping or laying out children)
@@ -28,23 +30,31 @@ import {
 // - TouchableOpacity (component goes see-thru when touched)
 // - TouchableHighlight (component goes dark when touched)
 
-export default function App() {
-  function handlePress() {
-    console.log('you pressed it');
-  }
+//Buttons are self-closing. Text in it is from title prop.
 
-  function handleLongPress() {
-    console.log('you pressed it for a long time');
-  }
+//alert as a function works similarly to browser - pops up a lil window with your message and then ok to close.
+//Alert as a component is imported with the other components, but:
+// - It's an object API with methods (but no visible component)
+// - Alert.alert('title', 'message', [{text: "button text", onPress: ()=>{}, {text: "other button text",  onPress: ()=>{}}])
+// - Alert.prompt('title', 'message', (text)=>{fx here that does something w/ the text the user inputted})
+
+export default function App() {
+  // function handlePress() {
+  //   console.log('you pressed it');
+  // }
+
+  // function handleLongPress() {
+  //   console.log('you pressed it for a long time');
+  // }
 
   return (
     // SafeAreaView adds padding on the top to leave room for the iPhone notch
     <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1} onPress={handlePress}>
+      {/* <Text numberOfLines={1} onPress={handlePress}>
         Hello React Native!
-      </Text>
+      </Text> */}
       {/*Image component doesn't have onPress! Need to wrap w/ separate touchable component depending on feedback we want to give to users. */}
-      <TouchableHighlight onPress={handlePress} onLongPress={handleLongPress}>
+      {/* <TouchableHighlight onPress={handlePress} onLongPress={handleLongPress}>
         <Image
           source={{
             uri: 'https://picsum.photos/200/300',
@@ -52,7 +62,32 @@ export default function App() {
             height: 200, //for network images, have to specify dimensions before it'll display
           }}
         />
-      </TouchableHighlight>
+      </TouchableHighlight> */}
+      <Button
+        color="orange"
+        title="Click Me"
+        onPress={() =>
+          Alert.alert('Stay alert!', 'Your country needs more lerts.', [
+            {
+              text: 'Um, okay.',
+              onPress: () => alert("You can't see a virus!"),
+            },
+            {
+              text: "No. That doesn't work for a virus.",
+              onPress: () => alert('Exactly!'),
+            },
+          ])
+        }
+      />
+      <Button
+        color="dodgerblue"
+        title="Click Me Next"
+        onPress={() =>
+          Alert.prompt('What do you think?', 'Tell me your thoughts.', (text) =>
+            alert(`You're thinking "${text}"`)
+          )
+        }
+      />
     </SafeAreaView>
   );
 }
